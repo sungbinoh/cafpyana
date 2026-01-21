cefbfc25d969f056ea3f3998594157ba4297fe07
sungbinoh <wwera6421@gmail.com>
2026-01-21 01:35:43 +0000

    shower bestplane to bestplane_for_energy and bestplane_for_dedx

--- makedf/branches.py
+++ makedf/branches.py
@@ -195,7 +195,7 @@ shwbranches = [
     shwbranch + 'conversion_gap', 
     shwbranch + "density",
     shwbranch + "open_angle",
-    shwbranch + 'bestplane',
+    shwbranch + 'bestplane_for_energy', shwbranch + 'bestplane_for_dedx',
     shwbranch + 'bestplane_dEdx', shwbranch + 'bestplane_energy',
     shwbranch + 'plane.0.dEdx',   shwbranch + 'plane.1.dEdx', shwbranch + 'plane.2.dEdx',
     shwbranch + 'plane.0.energy', shwbranch + 'plane.1.energy', shwbranch + 'plane.2.energy',
