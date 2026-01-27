0d3fe927ee6ac1b3babe610211b40b1ed812d609
Mun Jung Jung <munjung@sbndbuild03.fnal.gov>
2026-01-27 15:44:31 -0600

    premultiply g4 multisims as well

--- makedf/g4syst.py
+++ makedf/g4syst.py
@@ -9,6 +9,13 @@ g4_systematics = [
     'reinteractions_proton_Geant4'
 ]
 
-def g4syst(f, nuind):
-    return getsyst.getsyst(f, g4_systematics, nuind)
+
+def g4syst(f, nuind, multisim_nuniv=250, slim=False):
+    g4wgtdf = getsyst.getsyst(f, g4_systematics, nuind, multisim_nuniv=multisim_nuniv, slim=slim, slimname="G4")
+
+    if slim:  # keep only the multiplied "g4.univ_" columns
+        g4_cols = [c for c in g4wgtdf.columns if c[0] == "G4"]
+        g4wgtdf = g4wgtdf[g4_cols]
+        
+    return g4wgtdf
 
