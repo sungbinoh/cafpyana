90efbae3552e551d4b1bd5e89e55a605c7f8f8aa
Mun Jung Jung <munjung@sbndbuild03.fnal.gov>
2026-01-27 01:37:36 -0600

    all g4 knobs

--- makedf/g4syst.py
+++ makedf/g4syst.py
@@ -1,9 +1,12 @@
 from . import getsyst
 
 g4_systematics = [
-    "reinteractions_piminus_Geant4",
-    "reinteractions_piplus_Geant4",
-    "reinteractions_proton_Geant4"
+     'reinteractions_kminus_Geant4',
+    'reinteractions_kplus_Geant4',
+    'reinteractions_neutron_Geant4',
+    'reinteractions_piminus_Geant4',
+    'reinteractions_piplus_Geant4',
+    'reinteractions_proton_Geant4'
 ]
 
 def g4syst(f, nuind):
