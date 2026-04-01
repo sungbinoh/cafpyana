70dd2d39812908d5951498e35fe0c32b65b1b37f
lynnt20 <lynnt@uchicago.edu>
2026-04-01 10:30:32 -0500

    add more g4 systs and specify slim/nuniv arguments

--- makedf/g4syst.py
+++ makedf/g4syst.py
@@ -1,11 +1,14 @@
 from . import getsyst
 
 g4_systematics = [
+    "reinteractions_kplus_Geant4",
+    "reinteractions_kminus_Geant4",
+    "reinteractions_neutron_Geant4",
     "reinteractions_piminus_Geant4",
     "reinteractions_piplus_Geant4",
     "reinteractions_proton_Geant4"
 ]
 
-def g4syst(f, nuind):
-    return getsyst.getsyst(f, g4_systematics, nuind)
+def g4syst(f, nuind, multisim_nuniv=250,slim=False):
+    return getsyst.getsyst(f, g4_systematics, nuind, multisim_nuniv=multisim_nuniv, slim=slim)
 
