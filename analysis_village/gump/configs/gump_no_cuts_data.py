d97e0cc7af685bc248f2587d0b6be0ebb40660f5
Nathaniel <nathanielerowe@uchicago.edu>
2026-04-13 15:36:21 -0500

    add flash to config

--- analysis_village/gump/configs/gump_no_cuts_data.py
+++ analysis_village/gump/configs/gump_no_cuts_data.py
@@ -1,4 +1,4 @@
 from analysis_village.gump.makedf import *
 
-DFS = [make_pandora_no_cuts_df, make_hdrdf, make_triggerdf, make_potdf_bnb]
-NAMES = ["evt", "hdr", "trig", "bnb"]
+DFS = [make_pandora_no_cuts_df, make_hdrdf, make_triggerdf, make_potdf_bnb, make_opflashdf]
+NAMES = ["evt", "hdr", "trig", "bnb", "flash"]
