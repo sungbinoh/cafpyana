3b184d5f9e2c752c596ada6dc9bae796e2205ed6
VCLanNguyen <vclnguyen@ucsb.edu>
2026-04-16 00:28:16 +0000

    update hnl configs to apply preselection and save all pfps

--- analysis_village/hnl_nuee_nupi0/makedf/make_hnldf.py
+++ analysis_village/hnl_nuee_nupi0/makedf/make_hnldf.py
@@ -20,9 +20,12 @@ def make_hnldf_mcnu_wgt(f):
     df = make_hnldf_mcnu(f,include_weights=True)
     return df
 
-def make_hnldf_mcnu(f, include_weights=False,multisim_nuniv=100,slim=True):
-    
-    slcdf = make_hnldf(f)
+def make_hnldf_mcnu_nopreselect_savepfp(f):
+    df = make_hnldf_mcnu(f,applyPreselection=False, savePfp=True)
+    return df
+
+def make_hnldf_mcnu(f, include_weights=False,multisim_nuniv=100,slim=True,applyPreselection=True, savePfp=False):
+    slcdf = make_hnldf(f, applyPreselection=applyPreselection, savePfp=savePfp)
     mcdf = make_mcnudf_hnl(f,include_weights=include_weights,multisim_nuniv=multisim_nuniv,slim=slim)
     mcdf.columns = pd.MultiIndex.from_tuples([tuple(["slc", "truth"] + list(c)) for c in mcdf.columns])
     df = multicol_merge(slcdf.reset_index(), 
@@ -43,8 +46,12 @@ def make_hnldf_mevprtl_wgt(f):
     df = make_hnldf_mevprtl(f,include_weights=True)
     return df
 
-def make_hnldf_mevprtl(f, include_weights=False, multisim_nuniv=100,slim=True):
-    slcdf = make_hnldf(f)
+def make_hnldf_mevprtl_nopreselect_savepfp(f):
+    df = make_hnldf_mevprtl(f,applyPreselection=False, savePfp=True)
+    return df
+
+def make_hnldf_mevprtl(f, include_weights=False, multisim_nuniv=100,slim=True,applyPreselection=True, savePfp=False):
+    slcdf = make_hnldf(f, applyPreselection=applyPreselection, savePfp=savePfp)
     mcdf = make_mevprtldf(f,include_weights=include_weights,multisim_nuniv=multisim_nuniv,slim=slim)
     mcdf.columns = pd.MultiIndex.from_tuples([tuple(["slc", "prtl"] + list(c)) for c in mcdf.columns])
     df = multicol_merge(slcdf.reset_index(), 
@@ -61,8 +68,8 @@ def make_hnldf_mevprtl(f, include_weights=False, multisim_nuniv=100,slim=True):
 # Functions to read data
 #----------------------------------------------------------------------------------#
 
-def make_hnldf_data(f):
-    slcdf = make_hnldf(f)
+def make_hnldf_data(f, applyPreselection=True, savePfp=False):
+    slcdf = make_hnldf(f, applyPreselection=applyPreselection, savePfp=savePfp)
     # drop truth cols for data
     slcdf = slcdf.drop('tmatch', axis=1,level=1) # slc level
     slcdf = slcdf.drop('truth',  axis=1,level=2) # pfp level
@@ -79,6 +86,10 @@ def make_hnldf_data(f):
     df = df.set_index(slcdf.index.names, verify_integrity=True)
     return df
 
+def make_hnldf_data_nopreselect_savepfp(f):
+    df = make_hnldf_data(f, applyPreselection=False, savePfp=True)
+    return df
+    
 #----------------------------------------------------------------------------------#
 # Functions to read reconstruction (both MC/data)
 #----------------------------------------------------------------------------------#
@@ -86,7 +97,7 @@ def make_hnldf_data(f):
 def p_to_energy(p, mass):
     return np.sqrt(p**2 + mass**2)
 
-def make_hnldf(f):
+def make_hnldf(f, applyPreselection=False, savePfp=False, trackScore=0.51):
     det = loadbranches(f["recTree"], ["rec.hdr.det"]).rec.hdr.det
     DETECTOR = "SBND"
     
@@ -96,132 +107,118 @@ def make_hnldf(f):
     
     pfpdf = pfpdf.drop('pfochar',axis=1,level=1)
 
-    #SHOWER
-    #count number of showers per event before selecting primary and secondary shower
-    nshwdf = pfpdf[(pfpdf.pfp.trackScore < 0.5) & (pfpdf.pfp.shw.maxplane_energy > 0)].groupby(level=[0,1]).size().to_frame('n_shws') #count number of showers
-    nshwdf.columns = pd.MultiIndex.from_tuples([tuple(['slc'] + list(nshwdf.columns))]) #change nshws column to have multiindex with same structure as shwdf
-    slcdf = multicol_merge(slcdf, nshwdf,left_index=True,right_index=True,how="left",validate="one_to_one")
-    slcdf['slc','n_shws'] = slcdf['slc','n_shws'].fillna(0)
+    #Save all PFPs in a slice
+    if savePfp:
+        # Keep slcdf as the left/base table and expand rows with pfp-level info.
+        slcdf = multicol_merge(slcdf,
+                           pfpdf.reset_index(level='rec.slc.reco.pfp..index'),
+                           left_index=True,
+                           right_index=True,
+                           how="left",
+                           validate="one_to_many")
 
-    del nshwdf
+        pfp_idx_col = next(
+            c for c in slcdf.columns
+            if c == 'rec.slc.reco.pfp..index' or (isinstance(c, tuple) and len(c) > 0 and c[0] == 'rec.slc.reco.pfp..index')
+        )
+        slcdf = slcdf.set_index(pfp_idx_col, append=True)
+    
+    #Save only 2 primary showers in a slice
+    else:
+        #Make sure to count only valid PFPs so apply a PFP sanity filter
+        #1. valid trackScore > 0
+        pfpdf = pfpdf[(pfpdf.pfp.trackScore > 0)]
+        #2. valid shower Energy
+        pfpdf = pfpdf[(pfpdf.pfp.shw.bestplane_energy > 0)]
+        #3. valid start x/y/z
+        pfpdf = pfpdf[(pfpdf.pfp.shw.start.x != -999) & (pfpdf.pfp.shw.start.y != -999) & (pfpdf.pfp.shw.start.z != -999)]
+        #4. valid track length > 0
+        pfpdf = pfpdf[(pfpdf.pfp.trk.len > 0)]
 
-    ## primary shw candidate is shw pfp with highest energy, valid energy, and score < 0.5
-    shwdf = pfpdf[(pfpdf.pfp.trackScore < 0.5) & (pfpdf.pfp.shw.maxplane_energy > 0)].sort_values(pfpdf.pfp.index.names[:-1] + [('pfp','shw','maxplane_energy','','','')]).groupby(level=[0,1]).nth(-1)
-    # drop all columns that are from trk attributes
-    shwdf = shwdf.drop('trk',axis=1,level=1)
-    shwdf.columns = shwdf.columns.set_levels(['primshw'],level=0)
-    slcdf = multicol_merge(slcdf, shwdf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
+        #SHOWER
+        #count number of showers per event before selecting primary and secondary shower
+        nshwdf = pfpdf[(pfpdf.pfp.trackScore < trackScore)].groupby(level=[0,1]).size().to_frame('n_shws') #count number of showers
+        nshwdf.columns = pd.MultiIndex.from_tuples([tuple(['slc'] + list(nshwdf.columns))]) #change nshws column to have multiindex with same structure as shwdf
+        slcdf = multicol_merge(slcdf, nshwdf,left_index=True,right_index=True,how="left",validate="one_to_one")
+        slcdf['slc','n_shws'] = slcdf['slc','n_shws'].fillna(0)
+        del nshwdf
 
-    del shwdf
+        # primary shw candidate is shw pfp with highest energy, valid energy, and score < trackScore
+        shwdf = pfpdf[(pfpdf.pfp.trackScore < trackScore)].sort_values(pfpdf.pfp.index.names[:-1] + [('pfp','shw','bestplane_energy','','','')]).groupby(level=[0,1]).nth(-1)
+        # drop all columns that are from trk attributes
+        shwdf = shwdf.drop('trk',axis=1,level=1)
+        shwdf.columns = shwdf.columns.set_levels(['primshw'],level=0)
+        slcdf = multicol_merge(slcdf, shwdf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
+        del shwdf
 
-    ## secondary shower is shw pfp with second highest energy, valid energy, and score < 0.5 
-    shwsecdf = pfpdf[(pfpdf.pfp.trackScore < 0.5) & (pfpdf.pfp.shw.maxplane_energy > 0)].sort_values(pfpdf.pfp.index.names[:-1] + [('pfp','shw','maxplane_energy','','','')]).groupby(level=[0,1]).nth(-2)
-    shwsecdf = shwsecdf.drop('trk',axis=1,level=1)
-    shwsecdf.columns = shwsecdf.columns.set_levels(['secshw'],level=0)
-    slcdf = multicol_merge(slcdf, shwsecdf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
+        # secondary shower is shw pfp with second highest energy, valid energy, and score < 0.5 
+        shwsecdf = pfpdf[(pfpdf.pfp.trackScore < trackScore)].sort_values(pfpdf.pfp.index.names[:-1] + [('pfp','shw','bestplane_energy','','','')]).groupby(level=[0,1]).nth(-2)
+        shwsecdf = shwsecdf.drop('trk',axis=1,level=1)
+        shwsecdf.columns = shwsecdf.columns.set_levels(['secshw'],level=0)
+        slcdf = multicol_merge(slcdf, shwsecdf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
+        del shwsecdf
 
-    del shwsecdf
+        #TRACKS
+        #GENERAL TRACKS: track score > 0.5 & track length > 0 
+        trkdf = pfpdf[(pfpdf.pfp.trackScore > trackScore)]
+        ntrkdf = trkdf.groupby(level=[0,1]).size().to_frame('n_trks') #count number of tracks
+        ntrkdf.columns = pd.MultiIndex.from_tuples([tuple(['slc'] + list(ntrkdf.columns))]) #change ntrks column to have multiindex with same structure as trkdf
+        slcdf = multicol_merge(slcdf, ntrkdf,left_index=True,right_index=True,how="left",validate="one_to_one")
+        slcdf['slc','n_trks'] = slcdf['slc','n_trks'].fillna(0)
+        del ntrkdf
 
-    #TRACKS
-    #GENERAL TRACKS: track score > 0.5 & track length > 0 
-    trkdf = pfpdf[(pfpdf.pfp.trackScore > 0.5) & (pfpdf.pfp.trk.len > 0)]
-    ntrkdf = trkdf.groupby(level=[0,1]).size().to_frame('n_trks') #count number of tracks
-    ntrkdf.columns = pd.MultiIndex.from_tuples([tuple(['slc'] + list(ntrkdf.columns))]) #change ntrks column to have multiindex with same structure as trkdf
-    slcdf = multicol_merge(slcdf, ntrkdf,left_index=True,right_index=True,how="left",validate="one_to_one")
-    slcdf['slc','n_trks'] = slcdf['slc','n_trks'].fillna(0)
+        ##save only longest track
+        #trkdf = trkdf.sort_values(pfpdf.pfp.index.names[:-1] + [('pfp','trk','len','','','')]).groupby(level=[0,1]).nth(-1)
+        ## drop all columns that are from shw attributes
+        #trkdf = trkdf.drop('shw',axis=1,level=1)
+        #trkdf.columns = trkdf.columns.set_levels(['primtrk'],level=0)
+        #slcdf = multicol_merge(slcdf, trkdf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
+        #del trkdf
 
-    del ntrkdf
+        ##MUON-LIKE TRACKS:
+        #mudf = pfpdf[(pfpdf.pfp.trackScore > 0.5) & (pfpdf.pfp.trk.len > 10) & (pfpdf.pfp.trk.chi2pid.I2.chi2_muon < 20) & (pfpdf.pfp.trk.chi2pid.I2.chi2_proton > 85)]
+        #nmudf = mudf.groupby(level=[0,1]).size().to_frame('n_mu_trks') #count number of tracks
+        #nmudf.columns = pd.MultiIndex.from_tuples([tuple(['slc'] + list(nmudf.columns))])
+        #slcdf = multicol_merge(slcdf, nmudf,left_index=True,right_index=True,how="left",validate="one_to_one")
+        #slcdf['slc','n_mu_trks'] = slcdf['slc','n_mu_trks'].fillna(0)
+        #del nmudf
 
-    #save only longest track
-    trkdf = trkdf.sort_values(pfpdf.pfp.index.names[:-1] + [('pfp','trk','len','','','')]).groupby(level=[0,1]).nth(-1)
-    # drop all columns that are from shw attributes
-    trkdf = trkdf.drop('shw',axis=1,level=1)
-    trkdf.columns = trkdf.columns.set_levels(['primtrk'],level=0)
-    slcdf = multicol_merge(slcdf, trkdf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
+        #save only primary muon with the highest energy
+        #mu_mass = 0.1056583745 # GeV/c^2
+        #mup_column = ('pfp','trk','rangeP','p_muon','','')
+        #energy_column = ('pfp','trk','energy','','','')
+        #mudf[energy_column] = mudf.apply(lambda row: p_to_energy(row[mup_column], mu_mass), axis=1)
+        #mudf = mudf.sort_values(pfpdf.pfp.index.names[:-1] + [energy_column]).groupby(level=[0,1]).nth(-1)
+        # drop all columns that are from shw attributes
+        #mudf = mudf.drop('shw',axis=1,level=1)
+        #mudf.columns = mudf.columns.set_levels(['primmu'],level=0)
+        #slcdf = multicol_merge(slcdf, mudf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
+        #del mudf
 
-    del trkdf
+        #PROTON-LIKE TRACKS:
+        #prodf = pfpdf[(pfpdf.pfp.trackScore > 0.5) & (pfpdf.pfp.trk.len > 0) & (pfpdf.pfp.trk.chi2pid.I2.chi2_muon > 20) & (pfpdf.pfp.trk.chi2pid.I2.chi2_proton < 85)]
+        #nprodf = prodf.groupby(level=[0,1]).size().to_frame('n_proton_trks') #count number of tracks
+        #nprodf.columns = pd.MultiIndex.from_tuples([tuple(['slc'] + list(nprodf.columns))])
+        #slcdf = multicol_merge(slcdf, nprodf,left_index=True,right_index=True,how="left",validate="one_to_one")
+        #slcdf['slc','n_proton_trks'] = slcdf['slc','n_proton_trks'].fillna(0)
+        #del nprodf
 
-    #MUON-LIKE TRACKS:
-    mudf = pfpdf[(pfpdf.pfp.trackScore > 0.5) & (pfpdf.pfp.trk.len > 10) & (pfpdf.pfp.trk.chi2pid.I2.chi2_muon < 20) & (pfpdf.pfp.trk.chi2pid.I2.chi2_proton > 85)]
-    nmudf = mudf.groupby(level=[0,1]).size().to_frame('n_mu_trks') #count number of tracks
-    nmudf.columns = pd.MultiIndex.from_tuples([tuple(['slc'] + list(nmudf.columns))])
-    slcdf = multicol_merge(slcdf, nmudf,left_index=True,right_index=True,how="left",validate="one_to_one")
-    slcdf['slc','n_mu_trks'] = slcdf['slc','n_mu_trks'].fillna(0)
+        #save only primary proton with the highest energy
+        #pro_mass = 0.9382720813 # GeV/c^2
+        #prop_column = ('pfp','trk','rangeP','p_proton','','')
+        #prodf[energy_column] = prodf.apply(lambda row: p_to_energy(row[prop_column], pro_mass), axis=1)
+        #prodf = prodf.sort_values(pfpdf.pfp.index.names[:-1] + [energy_column]).groupby(level=[0,1]).nth(-1)
+        # drop all columns that are from shw attributes
+        #prodf = prodf.drop('shw',axis=1,level=1)
+        #prodf.columns = prodf.columns.set_levels(['primproton'],level=0)
+        #slcdf = multicol_merge(slcdf, prodf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
 
-    del nmudf
-
-    #save only primary muon with the highest energy
-    mu_mass = 0.1056583745 # GeV/c^2
-    mup_column = ('pfp','trk','rangeP','p_muon','','')
-    energy_column = ('pfp','trk','energy','','','')
-    mudf[energy_column] = mudf.apply(lambda row: p_to_energy(row[mup_column], mu_mass), axis=1)
-    mudf = mudf.sort_values(pfpdf.pfp.index.names[:-1] + [energy_column]).groupby(level=[0,1]).nth(-1)
-    # drop all columns that are from shw attributes
-    mudf = mudf.drop('shw',axis=1,level=1)
-    mudf.columns = mudf.columns.set_levels(['primmu'],level=0)
-    slcdf = multicol_merge(slcdf, mudf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
-
-    del mudf
-
-    #PROTON-LIKE TRACKS:
-    prodf = pfpdf[(pfpdf.pfp.trackScore > 0.5) & (pfpdf.pfp.trk.len > 0) & (pfpdf.pfp.trk.chi2pid.I2.chi2_muon > 20) & (pfpdf.pfp.trk.chi2pid.I2.chi2_proton < 85)]
-    nprodf = prodf.groupby(level=[0,1]).size().to_frame('n_proton_trks') #count number of tracks
-    nprodf.columns = pd.MultiIndex.from_tuples([tuple(['slc'] + list(nprodf.columns))])
-    slcdf = multicol_merge(slcdf, nprodf,left_index=True,right_index=True,how="left",validate="one_to_one")
-    slcdf['slc','n_proton_trks'] = slcdf['slc','n_proton_trks'].fillna(0)
-
-    del nprodf
-
-    #save only primary proton with the highest energy
-    pro_mass = 0.9382720813 # GeV/c^2
-    prop_column = ('pfp','trk','rangeP','p_proton','','')
-    prodf[energy_column] = prodf.apply(lambda row: p_to_energy(row[prop_column], pro_mass), axis=1)
-    prodf = prodf.sort_values(pfpdf.pfp.index.names[:-1] + [energy_column]).groupby(level=[0,1]).nth(-1)
-    # drop all columns that are from shw attributes
-    prodf = prodf.drop('shw',axis=1,level=1)
-    prodf.columns = prodf.columns.set_levels(['primproton'],level=0)
-    slcdf = multicol_merge(slcdf, prodf.droplevel(-1),left_index=True,right_index=True,how="left",validate="one_to_one")
-
-    del prodf
+        #del prodf
 
     # pre-selection cuts
-    slcdf = slcdf[slcdf.slc.is_clear_cosmic==0]
-    slcdf = slcdf[slcdf.slc.nu_score > 0.5]
-    slcdf = slcdf[InFV(df=slcdf.slc.vertex, inzback=0, det=DETECTOR)]    
-    
-    # recalc dEdx for the primary shower
-    # do after pre-selection to speed up
-    for plane in range(3):
-        trkhitdf = make_trkhitdf(f,plane)
-        slchitdf = multicol_merge(slcdf.reset_index(), 
-                                   trkhitdf.reset_index(),
-                                   left_on=[('entry', '', '', '', '', ''), 
-                                           ('rec.slc..index', '', '', '', '', ''),
-                                           ('primshw','tindex', '', '', '', '')], 
-                                   right_on=[('entry', '', '', '', '', ''), 
-                                           ('rec.slc..index', '', '', '', '', ''),
-                                           ('rec.slc.reco.pfp..index', '', '', '', '', ''),],
-                                   how="left")
-        slchitdf = slchitdf.set_index(trkhitdf.index.names,verify_integrity=True)
-        slchitdf = multicol_add(slchitdf,dmagdf(slchitdf.primshw.shw.start,slchitdf).rename("sp_to_start"))
-        
-        # require that the spacepoints are between 0.5 cm and 5 cm of the shower start
-        # require that the spacepoints are within the AV
-        slchitdf = slchitdf[(slchitdf.sp_to_start < 5) & (slchitdf.sp_to_start > 0.5)]
-        slchitdf = slchitdf[InAV(slchitdf)]
-
-        slchitdf['dedx_reco'] = chi2pid.dedx(slchitdf,gain="SBND",calibrate="SBND",plane=plane)
-        this_dedx_col = ('primshw','shw','plane',f'I{plane}','dEdx_new')
-        this_hits_col = ('primshw','shw','plane',f'I{plane}','nHits_dEdx')
-
-        slcdf = multicol_add(slcdf,slchitdf[('dedx_reco', '', '', '', '', '')].groupby(slchitdf.index.names[:-2]).median().rename(this_dedx_col), default=-999)
-        slcdf = multicol_add(slcdf,slchitdf[('dedx_reco', '', '', '', '', '')].groupby(slchitdf.index.names[:-2]).count().rename(this_hits_col),default=-999)
-    
-    slcdf['primshw','shw','maxplane_dEdx_new','','',''] = np.nan
-    slcdf['primshw','shw','maxplane_idx','','',''] = slcdf.loc(axis=1)['primshw','shw','plane',:,"nHits_dEdx"].idxmax(axis=1).apply(lambda x: x[3])
-
-    conditions = [slcdf['primshw','shw','maxplane_idx','','','']=="I2",slcdf['primshw','shw','maxplane_idx','','','']=="I1",slcdf['primshw','shw','maxplane_idx','','','']=="I0"]
-    choices = [slcdf['primshw','shw','plane','I2','dEdx_new',''],slcdf['primshw','shw','plane','I1','dEdx_new',''],slcdf['primshw','shw','plane','I0','dEdx_new','']]
-    slcdf['primshw','shw','maxplane_dEdx_new','','',''] = np.select(conditions,choices,default=np.nan)
+    if applyPreselection:
+        slcdf = slcdf[slcdf.slc.is_clear_cosmic==0]
+        slcdf = slcdf[slcdf.slc.nu_score > 0.5]
+        slcdf = slcdf[InFV(df=slcdf.slc.vertex, inzback=0, det=DETECTOR)]
     
     return slcdf 
