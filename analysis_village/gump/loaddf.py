2ebb3b3b45d871fbb06c50bca001e964fdde87c6
Gray Putnam <gputnam@fnal.gov>
2026-07-31 16:06:34 -0500

    Fix accidental merge bug.

--- analysis_village/gump/loaddf.py
+++ analysis_village/gump/loaddf.py
@@ -238,7 +238,6 @@ def _write_cache(cache_file, df, match, pot):
 def load_one(fname, idf,
     detector=None, # One of SBND, ICARUS, ICARUS Run4
     include_syst=True, nuniv=100, spline=False, xsec_univ=False, xsec_spline=False,# systematic handling
-    reweight_aFF=False, pot_univ=False, flux_univ=True, sep_flux_univ=False,
     reweight_aFF=False, pot_univ=False, flux_univ=True, sep_flux_univ=False, g4_univ=True,
     pot_spline=False, detvar_spline=False,
     load_truth=True, load_crt=False, match_Enu=True, # load extra information
