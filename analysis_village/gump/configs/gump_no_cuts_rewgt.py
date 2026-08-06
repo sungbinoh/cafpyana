41fa30f3560965967ac0355064e385a017c5467c
Gray Putnam <gputnam@fnal.gov>
2026-08-06 10:14:00 -0500

    Wire GENIE Event Record into evtrec DF for MC CV processing.

--- analysis_village/gump/configs/gump_no_cuts_rewgt.py
+++ analysis_village/gump/configs/gump_no_cuts_rewgt.py
@@ -3,5 +3,5 @@ from preprocess.preprocess import Script
 
 PREPROCESS = []
 
-DFS = [make_pandora_no_cuts_calosyst_df, make_gump_nudf, make_stubs, make_crthitdf, make_hdrdf, make_triggerdf, make_potdf_bnb, make_gump_nurewgtdf, make_opflashdf]
-NAMES = ["evt", "mcnu", "stub", "crt", "hdr", "trig", "bnb", "wgt", "flash"]
+DFS = [make_pandora_no_cuts_calosyst_df, make_gump_nudf, make_stubs, make_crthitdf, make_hdrdf, make_triggerdf, make_potdf_bnb, make_gump_nurewgtdf, make_opflashdf, make_genie_evtrec_df]
+NAMES = ["evt", "mcnu", "stub", "crt", "hdr", "trig", "bnb", "wgt", "flash", "evtrec"]
